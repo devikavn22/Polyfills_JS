1b3e41f765be2bf1c66d18e2a1cac92d62277ef7
devikavn22 <devika1997eee@gmail.com>
2025-03-27 10:35:24 +0530

    Filter Polyfill

--- filterPolyfills.js
+++ filterPolyfills.js
@@ -2,7 +2,7 @@
 Array.prototype.myFilter=function(cb){
     let temp=[];
 
-    for(i=0; i<this.length;i++){
+    for(let i=0; i<this.length;i++){
         if(cb(this[i],i,this)) temp.push(this[i]);
     }
     return temp;
